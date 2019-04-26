9b30aa6681ed428fd109ada538fd3f640405df3d
xhspdle <xhspdle@naver.com>
2019-04-26 15:57:05 +0900

    작업주웅~

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 const mysql = require('mysql');
 const connection = mysql.createConnection({
@@ -17,7 +18,7 @@ router.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
 });
 
-router.get('/slideshow', (req, res) => {
+router.get('/slideshow_MySQL', (req, res) => {
     let sql = 'SELECT * FROM slide_show';
     connection.query(sql, (err, result) => {
         if(err){
@@ -28,6 +29,18 @@ router.get('/slideshow', (req, res) => {
             res.status(200).send(result);
         }
     });
+});
+
+/*
+    간단하게 static.json파일 자체를 db로 관리하는 방법도 있다
+    C : json파일에 직접 write
+    R : json파일 읽기
+    U : json파일에 직접 update
+    D : json파일에 직접 delete
+    !!주의 이렇게 하면 서버 패치할때 json파일도 함께 백업하고 관리해줘야한다
+*/
+router.get('/slideshow', (req, res) => {
+    res.sendFile(path.join(__dirname, 'data', 'static.json'));
 });
 
 module.exports = router;
