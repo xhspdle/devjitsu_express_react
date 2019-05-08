dfac6c3dc1bc78a862570e43c5fb86636b4fc76e
xhspdle <xhspdle@naver.com>
2019-05-08 17:46:59 +0900

    커밋커밋

--- app.js
+++ app.js
@@ -12,23 +12,24 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {
-        /*
-        빌드되는거 보고 아래 두개중에 하나 선택해서 써야댐
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-        res.sendFile(path.resolve(__dirname, 'build', 'public', 'index.html'));
-        */
-    });
-}
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
+
+console.log('[[ process.env.NODE_ENV ]] : ' + process.env.NODE_ENV);
+/*
+    NODE_ENV 셋팅 방법
+    - powershell: $env:NODE_ENV="production"
+*/
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next){
